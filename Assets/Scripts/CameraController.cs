1c98a6e753565a54552b4a92838ad5fc90a7b6ec
SergeRichard <serge.richard@rogers.com>
2016-10-15 13:32:42 -0300

    Player moves, jumps, animates, and camera follows player with camera look ahead added.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public GameObject target;
+	public float followAhead;
+
+	public float smoothing;
+
+	private Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetPosition = new Vector3 (target.transform.position.x, transform.position.y, transform.position.z);
+
+		if (target.GetComponent<SpriteRenderer>().flipX == false) {
+			targetPosition = new Vector3 (targetPosition.x + followAhead, targetPosition.y, targetPosition.z);
+		} else {
+			targetPosition = new Vector3 (targetPosition.x - followAhead, targetPosition.y, targetPosition.z);
+		}
+
+		//transform.position = targetPosition;
+
+		transform.position = Vector3.Lerp (transform.position, targetPosition, smoothing * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	private Rigidbody2D myRigidbody;
+
+	public float jumpSpeed;
+
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+
+	public bool isGrounded;
+
+	private Animator myAnim;
+	private SpriteRenderer mySpriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D> ();
+		myAnim = GetComponent<Animator> ();
+		mySpriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isGrounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+
+		if (Input.GetAxisRaw ("Horizontal") > 0f) {
+			myRigidbody.velocity = new Vector3 (moveSpeed, myRigidbody.velocity.y, 0f);
+			mySpriteRenderer.flipX = false;
+		} else if (Input.GetAxisRaw ("Horizontal") < 0f) {
+			myRigidbody.velocity = new Vector3 (-moveSpeed, myRigidbody.velocity.y, 0f);
+			mySpriteRenderer.flipX = true;
+		} else {
+			myRigidbody.velocity = new Vector3 (0f, myRigidbody.velocity.y, 0f);
+		}
+		if (Input.GetButtonDown ("Jump") && isGrounded) {
+			myRigidbody.velocity = new Vector3 (myRigidbody.velocity.x, jumpSpeed, 0f);
+		}
+
+		myAnim.SetFloat ("Speed", Mathf.Abs(myRigidbody.velocity.x));
+		myAnim.SetBool ("Grounded", isGrounded);
+	}
+}
